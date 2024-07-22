700e9d774b89e706e8d7cdd10ffe095e1a9de82d
Quangnpph27509 <146161996+Quangnpph27509@users.noreply.github.com>
2024-07-22 15:41:10 +0700

    config

---
+++ DATN/rest-api/src/config/ConnectDB.ts
@@ -0,0 +1,17 @@
+import path from "path";
+import { Sequelize } from "sequelize-typescript";
+
+export async function connectDB() {
+  const sequelize = new Sequelize({
+    logging: true,
+    dialect: "mysql",
+    host: process.env["DB_HOST"],
+    database: process.env["DB_NAME"],
+    password: process.env["DB_PASSWORD"],
+    username: process.env["DB_USER"],
+    models: [path.resolve("./src/models")],
+  });
+  await sequelize.authenticate();
+  await sequelize.sync({ force: false });
+  console.log("Connection has been established successfully.");
+}
