c42db99338b5d8b522e9415d06074854c2b80ad5
the1411 <thethe141103@gmail.com>
2024-08-04 01:19:26 +0700

    comit lại 8/4

--- DATN/rest-api/src/app.ts
+++ DATN/rest-api/src/app.ts
@@ -1,31 +1,69 @@
-
-import "dotenv/config";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 import cors from "cors";
-import express from "express";
-import sequelize from "./config/ConnectDB copy";
-import {appRouter} from "./router/appRouter";
+import "dotenv/config";
+import express, { Request, Response } from "express";
+import { Send } from "express-serve-static-core";
+import { connectDB } from "./config/ConnectDB";
+import { redis } from "./config/ConnectRedis";
+import { RESPONSE_CODE, ResponseBody, STATUS_CODE } from "./constants";
+import { appRouter } from "./router/appRouter";
 
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: number;
+    }
+    interface Response {
+      json: Send<
+        {
+          code: number;
+          data: any;
+          message: string;
+        },
+        this
+      >;
+    }
+  }
+}
 
 export const app = express();
 
 app.use(
   cors({
-  origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3001", "http://localhost:3000"],
     credentials: true,
   })
 );
 
 app.use("/public", express.static("public"));
 
+redis.initial();
+
+connectDB();
+
+//PUBLIC ROUTER
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 appRouter();
 
-// app.use("*", (req, res) => {
-//   res.status(404).json({ mess: "404 Not Found" });
-// });
+app.use((errors: any, _: Request, res: Response) => {
+  res.json(errors);
+});
 
-
+app.use("*", (_, res) => {
+  res.status(STATUS_CODE.NOT_FOUND).json(
+    ResponseBody({
+      code: RESPONSE_CODE.ERRORS,
+      message: "Đường dẫn không tồn tại",
+    })
+  );
+});
 
 app.listen(process.env.SERVER_PORT, () =>
-  console.log("The server is running on port: http://localhost:" + process.env.SERVER_PORT)
+  console.log("The server is running on port:" + process.env.SERVER_PORT)
 );
