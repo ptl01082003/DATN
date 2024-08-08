3ef3260c3c59ab2930b9b2b4e4e6f3bc3f43a9d7
longpt <lbeephamthanhlong@gmail.com>
2024-08-08 23:48:56 +0700

    cau hinh req,res

--- DATN/rest-api/src/constants/index.ts
+++ DATN/rest-api/src/constants/index.ts
@@ -1,30 +1,31 @@
-import { RedisClientType } from "@redis/client";
-import { createClient } from "redis";
-
-class Redis {
-  public redis!: RedisClientType;
-  async initial() {
-    try {
-      this.redis = createClient();
-      await this.redis.connect();
-      console.log("create new a connection to redis server");
-    } catch (error) {
-      console.log("error connecting to redis server:: ", error);
-    }
-  }
-  async get(key: string): Promise<string | null> {
-    const value = await this.redis.get(key);
-    return value;
-  }
-  async set(key: string, value: string) {
-    await this.redis.set(key, value);
-  }
-  async setEx(key: string, value: string) {
-    await this.redis.set(key, value);
-  }
-  async del(key: string) {
-    await this.redis.del(key);
-  }
+export enum RESPONSE_CODE {
+  SUCCESS = 0,
+  ERRORS = 1,
+  NOT_FOUND = 404,
+  NOT_AUTHOR = 83,
+  NOT_AUTHEN = 85,
+  INCORRECT = 86,
 }
 
-export const redis = new Redis();
+export enum STATUS_CODE {
+  SUCCESS = 200,
+  NOT_AUTHEN = 401,
+  NOT_AUTHOR = 403,
+  NOT_FOUND = 404,
+}
+
+type ResponseBodyType<T> = {
+  code: RESPONSE_CODE;
+  message: string;
+  data?: any;
+};
+
+export function ResponseBody<T>({
+  code = RESPONSE_CODE.SUCCESS,
+  ...rest
+}: ResponseBodyType<T>): ResponseBodyType<T> {
+  return {
+    code,
+    ...rest,
+  };
+}
